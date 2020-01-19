ad0f934adf92cb0e258ddb09b7c1c8e1072bd5e6
Nahid <nahidshaiket10300@gmail.com>
2020-01-19 16:15:17 +0600

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Nahid on 19/1/20.
+//  Copyright © 2020 Nahid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
